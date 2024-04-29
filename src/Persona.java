9615092287b0e796b6aa0b3638c7968febf83155
emoreno84 <117529696+emoreno84@users.noreply.github.com>
2024-04-29 09:00:53 +0200

    se añade clase persona

---
+++ src/Persona.java
@@ -0,0 +1,34 @@
+public class Persona {
+    private String nombre;
+
+    private int edad;
+    private int altura;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+
+
+
+}
