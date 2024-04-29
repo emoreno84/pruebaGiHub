70d963001c9347333d929271d9de50194d13fc26
emoreno84 <emorenomor4@educacion.navarra.es>
2024-04-29 08:58:10 +0200

    se añade la clase animal

---
+++ src/Animal.java
@@ -0,0 +1,29 @@
+public class Animal {
+    private String especie;
+
+    private int numPatas;
+    private String habitat;
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public int getNumPatas() {
+        return numPatas;
+    }
+
+    public void setNumPatas(int numPatas) {
+        this.numPatas = numPatas;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+}
